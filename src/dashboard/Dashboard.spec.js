0a063ee91b255d66b4cba69ff9bc9e72c528cbf8
Aaron Beasley <aaroncb@aaroncb.com>
2020-01-01 03:53:03 -0600

    Added redux testing to app.

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,12 +1,34 @@
-import React from "react";
-import { render, fireEvent, act, getByText } from "@testing-library/react";
+import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import { initialState, rootReducer } from '../reducers/reducers'
+
 import Dashboard from './Dashboard';
 
-const dash = render(<Dashboard/>)
+function renderWithRedux(
+    ui,
+    { initialState, store = createStore(rootReducer, initialState) } = {}
+  ) {
+    return {
+      ...render(<Provider store={store}>{ui}</Provider>),
+      // adding `store` to the returned utilities to allow us
+      // to reference it in our tests (just try to avoid using
+      // this to test implementation details).
+      store,
+    }
+  }
 
-describe('<Dashboard />', () => {
-    it('<Dashboard /> renders', () => {
-        dash;
-    });
-});
+  test('can render with redux with defaults', () => {
+    const { getByTestId, getByText } = renderWithRedux(<Dashboard />)
+    expect(getByTestId('locked')).toHaveTextContent('Unlocked')
+  })
+
+  test('can render with redux with custom initial state', () => {
+    const { getByTestId, getByText } = renderWithRedux(<Dashboard />, {
+      initialState: { unlocked: true,
+        open: true },
+    })
+    expect(getByTestId('locked')).toHaveTextContent('Locked')
+  })
